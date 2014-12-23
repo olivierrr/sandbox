9d58070d09cbe48df1defb97a485c82aaf00155b
randy brown <randy@beingbrown.net>
2014-12-22 20:09:51 -0800

    lame solution to image issue

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,12 @@
 window.onload = function() {
-  var map = new Tilemap("assets/terrain.png",32,32)
+  var map = new Tilemap("assets/terrain.png",32,32,render)
+}
+
+function render(map) {
+  var k = Math.floor(Math.random() * map.tiles.length)
+
+  console.log(k)
+  console.log(map.tiles[k])
 
   var canvas = document.getElementById("canvas")
   canvas.width = window.innerWidth
@@ -7,23 +14,28 @@ window.onload = function() {
 
   var context = canvas.getContext("2d")
 
+  context.drawImage(map.img,map.tiles[k].x,map.tiles[k].y,
+                                map.tileWidth,map.tileHeight,
+                            0,0,map.tileWidth,map.tileHeight)
+}
 
-function Tilemap(image,tileWidth,tileHeight) {
+function Tilemap(image,tileWidth,tileHeight,callback) {
   this.img = new Image()
-  this.img.src = image
 
   this.tileWidth = tileWidth
   this.tileHeight = tileHeight
 
-  console.log(this.img.width)
-
-  this.tiles = []
-  for(var i = 0; i < this.img.width; i+=tileWidth) {
-    for(var j = 0; j < this.img.height; j+=tileHeight) {
-      this.tiles.push(new Tile(i,j))
+  this.img.onload = function() {
+    this.tiles = []
+    for(var i = 0; i < this.img.width; i+=tileWidth) {
+      for(var j = 0; j < this.img.height; j+=tileHeight) {
+        this.tiles.push(new Tile(i,j))
+      }
     }
-  }
-  console.log(this.tiles)
+    callback(this)
+  }.bind(this)
+
+  this.img.src = image
 }
 
 function Tile(x,y) {
